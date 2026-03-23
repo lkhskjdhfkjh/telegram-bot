f2ac030a4f9ca8f6d4857b9e36a1e66fb3185b84
lkhskjdhfkjh <nikitenkomilena100@gmail.com>
2026-03-23 14:58:35 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,15 +1,16 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-
 import os
+
 TOKEN = os.getenv("8699261089:AAHUKDhXNgUVkHNyOpkXHatlTRUZzM53n4U")
 
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Я живий 😎")
+    await update.message.reply_text("ПРИВІТ")
+
 
 app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(CommandHandler("start", start))
 
-print("STARTED")
+app.add_handler(CommandHandler("start", start))
 
 app.run_polling()
